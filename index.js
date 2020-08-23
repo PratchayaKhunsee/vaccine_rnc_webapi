99e98066793daf9417c151e430739be207577cf5
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-08-23 20:30:52 +0700

    TEST#10

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let client = require('./database').client;
 let app = express();
+let port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
     res.send('Successful.');
 });
 
-app.listen(443, function(){
+app.listen(port, function(){
     console.log('Have a vistor');
 });
