6dc26f03f3c1be0f657b6cc6d3a0b2e2c2390984
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-08-23 20:17:28 +0700

    Test#4

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import express from 'express';
-import client from './database';
+let express = require('express');
+let client = require('./database');
+let app = express();
+
+app.get('/', function(req, res) {
+    res.write('Successful');
+    res.end();
+})
