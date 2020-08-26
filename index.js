5f5801774178733ab47f718ae1bc7a1cefe98747
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-08-27 00:42:56 +0700

    Using login authentication

--- index.js
+++ index.js
@@ -14,27 +14,22 @@ let port = process.env.PORT || 8080;
 
 
 // configure passport.js to use the local strategy
-// passport.use(new LocalStrategy({
-//         usernameField: 'email'
-//     },
-//     (email, password, done) => {
-//         console.log('Inside local strategy callback')
-//         // here is where you make a call to the database
-//         // to find the user based on their username or email address
-//         // for now, we'll just pretend we found that it was users[0]
-//         const user = users[0]
-//         if (email === user.email && password === user.password) {
-//             console.log('Local strategy returned true')
-//             return done(null, user)
-//         }
-//     }
-// ));
+passport.use(new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    (username, password, done) => {
+        (async () => {
+            done(null, await (await login(username, password)))
+        })();
+    }
+));
 
 // tell passport how to serialize the user
-// passport.serializeUser((user, done) => {
-//     console.log('Inside serializeUser callback. User id is save to the session file store here')
-//     done(null, user.id);
-// });
+passport.serializeUser((user, done) => {
+    // console.log('Inside serializeUser callback. User id is save to the session file store here')
+    done(null, user.id);
+});
 
 // Setting middlewares for the app.
 app.use(session({
@@ -63,20 +58,10 @@ app.post('/login', function (req, res) {
         });
 
         try {
-            let loginSuccess = await (await login(req.body.username, req.body.password));
-            if (loginSuccess instanceof LoginError) {
-                res.send("false");
-                return;
-            }
+            passport.authenticate('local', function(){
+                console.log(arguments)
+            });
 
-
-            // await new Promise(function(yes, no){
-            //     passport.authenticate('local', (err, user, info) => {
-            //         req.login(user);
-            //         yes();
-            //     });
-            // });
-            
             res.send("true");
         } catch (error) {
             res.send("false");
