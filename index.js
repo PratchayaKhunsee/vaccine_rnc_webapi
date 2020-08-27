09d4b1b3e5858e3ae0e6db7f0f12629882f90f65
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-08-27 19:51:03 +0700

    Simple but Effective

--- index.js
+++ index.js
@@ -19,16 +19,13 @@ passport.use(new LocalStrategy({
     },
     function verify(username, password, done) {
         (async () => {
-            try {
-                let result = await (await login(username, password));
-                if (result instanceof LoginError) {
-                    throw result;
-                }
-                
+            let result = await (await login(username, password));
+            if (result instanceof LoginError) {
                 done(null, result);
-            } catch (err) {
-                done(result);
+                return;
             }
+
+            done(result);
         })();
     }
 ));
@@ -61,23 +58,16 @@ app.post('/login', function (req, res, next) {
     res.set({
         'Content-Type': 'application/json'
     });
-
-    (async () => {
-        try {
-            passport.authenticate('local', function (err, user) {
-                if (err) {
-                    throw err;
-                }
-
-                req.login(user, function done() {
-                    res.send("true");
-                });
-            })(req, res, next);
-
-        } catch (error) {
+    
+    passport.authenticate('local', function (err, user) {
+        if (err) {
             res.send("false");
         }
-    })();
+
+        req.login(user, function done() {
+            res.send("true");
+        });
+    })(req, res, next);
 });
 
 app.listen(port, function () {});
