bcd249311064d6d82ad59ab6a79c1029f61a9acc
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-09-23 17:59:46 +0700

    QuickFix: User account modification.

--- query/user.js
+++ query/user.js
@@ -104,13 +104,13 @@ async function editUser(client, username, info, password) {
 
     // Password updating
     if(password){
-        console.log(password.old, password.new, username);
-        console.log(await client.query(`SELECT * FROM user_account WHERE username = $1 AND password = $2`,[
-            username,
-            password.old
-        ]))
+        // console.log(password.old, password.new, username);
+        // console.log(await client.query(`SELECT * FROM user_account WHERE username = $1 AND password = $2`,[
+        //     username,
+        //     password.old
+        // ]))
         let modified = await client.query(
-            `UPDATE user_account SET password = $1 WHERE username = $2 AND password = $3`,
+            `UPDATE user_account SET password = crypt($1, gen_salt('md5')) WHERE username = $2 AND password = crypt($3, password)`,
             [
                 password.new,
                 username,
