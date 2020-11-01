850d55ec8100dbf0ec8528ae7350a5113404badb
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-11-01 13:59:35 +0700

    Da!

--- query/certificate.js
+++ query/certificate.js
@@ -393,6 +393,7 @@ async function viewCertificate(client, username, selection) {
  */
 async function editCertificate(client, username, certificate) {
     try {
+        console.log(certificate);
         await client.query('BEGIN');
 
         let checkUser = await checkUserName(client, username);
@@ -428,7 +429,7 @@ async function editCertificate(client, username, certificate) {
 
         return result;
     } catch (error) {
-        console.log(error);
+        
         await client.query('ROLLBACK');
         return new ErrorWithCode(error);
     }
