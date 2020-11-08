d74bebfa70b76abcd6a9e7738d447fcca4a641c4
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2020-11-08 17:30:40 +0700

    Please work

--- query/certificate.js
+++ query/certificate.js
@@ -627,6 +627,7 @@ async function editCertificateHeader(client, username, context) {
 
         return result;
     } catch (error) {
+        console.log(error);
         await client.query('ROLLBACK');
         return new ErrorWithCode(error);
     }
