cee32db18145935efd4e6376f1b5462b441f43c4
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2021-08-30 15:08:34 +0700

    Damn

--- src/multipart.js
+++ src/multipart.js
@@ -112,7 +112,7 @@ class MultipartField {
 
         intArray.push(0x0d, 0x0a, 0x0d, 0x0a);
 
-        // console.log(this.#fieldname, 'isFile =', this.#isFile(), payload);
+        console.log(this.#fieldname, 'isFile =', this.#isFile(), payload);
         if (this.#isFile()) {
 
             intArray.push(...Buffer.from(payload, 'utf-8'));
