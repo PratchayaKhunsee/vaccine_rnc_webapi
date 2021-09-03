d5a1a45100642a4acb90d5928d2a5726b6fcef85
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2021-09-03 19:40:52 +0700

    Damn

--- index.js
+++ index.js
@@ -733,7 +733,6 @@ App.route({
                                 /** @type {(import('./src/multipart').MultipartField)[]}*/
                                 const list = reader.get();
                                 for (let o of list) {
-                                    console.log(o.name, o.value);
                                     switch (o.name) {
                                         case 'clinician_signature':
                                         case 'administring_centre_stamp':
